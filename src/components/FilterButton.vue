55db841fd45ea0629445bdc0ab1e95a28e080501
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2025-04-01 04:44:46 +0800

    Add FilterButton component for task filtering functionality

---
+++ src/components/FilterButton.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import type { TaskFilter } from "../types";
+
+const { filter, currentFilter } = defineProps<{
+    filter: TaskFilter;
+    currentFilter: TaskFilter;
+}>();
+
+const emits = defineEmits<{
+    setFilter: [filter: TaskFilter];
+}>();
+</script>
+
+<template>
+    <button
+        class="secondary"
+        @click="emits('setFilter', filter)"
+        :class="{
+            contrast: currentFilter === filter,
+        }"
+    >
+        {{ filter }}
+    </button>
+</template>
+
+<style scoped>
+button {
+    text-transform: capitalize;
+}
+</style>
